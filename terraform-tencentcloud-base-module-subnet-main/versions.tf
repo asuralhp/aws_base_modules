4b584ea1e6effce1faa3d6548dfe7306ef03f1e0
asuralhp <asuralhp@gmail.com>
2025-12-05 12:00:12 +0800

    tc_subnet_move

--- terraform-tencentcloud-base-module-subnet-main/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    tencentcloud = {
-      source  = "tencentcloudstack/tencentcloud"
-      version = "~> 1.82.0"
-    }
-  }
-}
