1030006d3655c197fc87f88525c0ca20435669a4
asuralhp <asuralhp@gmail.com>
2025-12-09 17:52:14 +0800

    provider_version

--- test.tf
+++ test.tf
@@ -1,5 +1,13 @@
-provider "aws" {
-	region = "ap-east-1"
+
+terraform {
+  required_version = ">= 1.10.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0.0"
+    }
+  }
 }
 
 resource "aws_vpc" "test" {
